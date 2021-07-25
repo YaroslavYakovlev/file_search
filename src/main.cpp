6864bb8506570a1c3eb2c39683e25b1bb56cc1a4
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-07-25 20:38:06 +0500

    предворительное решение

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(){
   std::cout << "File search" << std::endl;
+  std::ifstream word;
+  std::vector<std::string> list;
+  std::string searchWord;
+  std::string str;
+  int countWord = 0;
+
+  std::cin >> searchWord;
+  word.open("D:\\ProjectCPP\\Module19\\file_search\\src\\File\\words.txt");
+  while(!word.eof()){
+      word >> str;
+      list.push_back(str);
+  }
+  for(int i = 0; i < list.size(); ++i){
+    if(searchWord == list[i]){
+      countWord++;
+      list[i] = str;
+    }
+  }
+    if(countWord > 0){
+      std::cout << "Word found - " << str << std::endl << "Number of repetitions: " <<  countWord;
+    }else{
+       std::cout << "The word was not found." << std::endl << "Number of repetitions: " <<  countWord;
+    }
+  word.close();
   
   return 0;
 }
